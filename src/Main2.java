292506362ce7027a2604f3b0fa5cb073eed3f8ae
kislota163 <119593358+kislota163@users.noreply.github.com>
2023-01-22 23:24:46 +0400

    Update Main2.java

--- src/Main2.java
+++ src/Main2.java
@@ -1,13 +1,15 @@
-public class Main {
+public class miles {
     public static void main(String[] args) {
         int sum = 1547;
         int con = 1000;
         int bonus = 0;
         if (sum < con) {
             System.out.println("Бонус:" + (bonus));
+            System.out.println("Итоговая сумма: " + (bonus));
         } else {
             bonus = (sum / 100);
             System.out.println("Бонус:" + (bonus));
+            System.out.println("Итоговая сумма: " + (bonus + sum));
         }
     }
 }
